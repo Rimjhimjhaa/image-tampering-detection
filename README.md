18b12bb48bd374b5a7b24c89eb608e2fba6f87cc
Rimjhim Jha <your_github_rj05jharimjhim@gmail.com>
2026-02-22 21:47:29 +0530

    Initial project upload

---
+++ README.md
@@ -0,0 +1,113 @@
+# Image Tampering Detection Using ELA and Metadata Analysis
+
+Image forensics has witnessed significant growth in recent years, driven by advancements in computer vision and the surge of digital data. Ensuring the authenticity of images has become a top priority, as sophisticated manipulation techniques continue to emerge. We propose a multi-modal approach to gain insight into the image's authenticity.
+
+### Try it on Streamlit
+You can try the live application here.\
+[Live Demo](https://imagetamperingdetection.streamlit.app/)
+#### Step 1
+Choose the image
+<p align="center">
+  <img src="imgs/step1.jpg" alt="Step 1" width="50%" height="50%">
+</p>
+
+#### Step 2
+Select if weather metadata is available
+<p align="center">
+  <img src="imgs/step2.jpg" alt="Step 2" width="50%" height="50%">
+</p>
+
+#### Step 3
+Analyze the results
+<p align="center">
+  <img src="imgs/step3.jpg" alt="Step 3" width="50%" height="50%">
+</p>
+ 
+
+### Running Locally
+Clone the repo<br>
+```bash
+git clone https://github.com/jayant1211/Image-Tampering-Detection-using-ELA-and-Metadata-Analysis.git
+cd Image-Tampering-Detection-using-ELA-and-Metadata-Analysis/
+```
+
+to install all dependencies, create a new virtualenv, and install all required packages as:<br>
+```bash
+pip install -r reuqirements
+```
+
+Usage:<br>
+Keep the model in <i>ELA_Training</i> Folder<br>
+run <code>streamlit run app.py</code> for local inference<br>
+
+### A Short Summary
+We are using ELA and Metadata Analysis to achieve insight into the authenticity of an image<br>
+#### 1. ELA
+when a lossy algorithm like JPEG compresses an image, the compression process introduces artifacts or discrepancies in it. these can appear as blocks or regions within an image, exhibiting pixel values that differ from those of the surrounding areas. when an image goes under manipulation, compression artifacts are disrupted for the tampered region.<br>
+
+in ELA, we calculate the absolute mean of an image at different compression levels:
+<p align="center">
+  <img src="https://github.com/jayant1211/Image-Tampering-Detection-using-ELA-and-Metadata-Analysis/blob/main/rsc/ela.jpg" alt="ELA Real Image" width="75%" height="75%">
+</p>
+<p align="center">ELA</p>
+by doing this, we are essentially amplifying the variations caused by compression artifacts.
+<p align="center">
+  <img src="https://github.com/jayant1211/Image-Tampering-Detection-using-ELA-and-Metadata-Analysis/blob/main/rsc/fake_img.jpg" alt="Fake Image" width="75%" height="75%">
+</p>
+<p align="center">ELA for fake image</p>
+The CASIA2.0 dataset contains a set of real and tampered images, we have used this dataset, and it is pre-processed to produce the ELA of every image(optimal image quality for compression level for calculating absolute diff was 90%). This preprocessed dataset is then trained on DenseNet121.
+<br>
+<br>
+
+#### 2. Weather Validation using Metadata Analysis
+image contains a lot of metadata with it, say, camera model, date, time, location, etc. By 'weather validation' to gain insight into the authenticity of an image, we mean precisely validating the depicted weather. A trained Weather CNN detects weather depicted in an image(preferably outdoor), and this result of Weather CNN is validated using Historical weather data. for fetching weather data all you need is a good open-source weather database, place, date, and time. Using metadata analysis, we could extract longitude and latitude, as well as the date and time. then parsing this metadata, we can send a request to weather-API to get the original weather on that place on a given date and time and validate our weather-CNN's result.
+<br>The dataset for training weather-CNN was collected from various sources. We have collected a total of 1,804 training images and 451 validation images, and the categories we narrowed down for the classification are the following:
+<ul>
+<li> Lighting
+<li> rainy
+<li> cloudy
+<li> sunny
+</ul>
+
+### Training
+In case you want to retrain the ELA models, download the CASIA2.0 Dataset and put it inside ELA_Training and run <code>main.ipynb</code>. If you want to access the weather dataset, you can contact me.
+
+### Results
+For ELA with DenseNet, using standard practices for training and optimizing the model, the accuracies model achieved were:
+| Metric                | Accuracy  |
+|-----------------------|----------:|
+| Train Accuracy        |   98.34%  |
+| Validation Accuracy   |   93.78%  |
+| Test Accuracy         |   87.24%  |
+
+For Weather CNN:<br>
+| Metric                | Accuracy  |
+|-----------------------|----------:|
+| Train Accuracy        |   91.2%   |
+| Validation Accuracy   |   81.6%   |
+| Test Accuracy         |   73.4%   |
+
+### Video
+[Video Result](https://youtu.be/aEpDw_GZb9g)
+
+### To-Dos
+- [ ] Use scene classification model to remove user dependency for checking whether the image is outdoor or not. (In progress)
+- [ ] Integration of Web-Traces and more modalities to Improve upon the Results.
+
+## Cite
+If you use our study in your research, please consider citing us, Thanks:
+
+<h3>BibTeX Citation</h3>
+
+  <pre><code>
+@INPROCEEDINGS{10169948,
+  author={Madake, Jyoti and Meshram, Jayant and Mondhe, Ajinkya and Mashalkar, Pruthviraj},
+  booktitle={2023 4th International Conference for Emerging Technology (INCET)}, 
+  title={Image Tampering Detection Using Error Level Analysis and Metadata Analysis}, 
+  year={2023},
+  volume={},
+  number={},
+  pages={1-7},
+  doi={10.1109/INCET57972.2023.10169948}}
+
+  </code></pre>
---
+++ app.py
@@ -0,0 +1,163 @@
+import streamlit as st
+import cv2
+import os
+import numpy as np
+from keras.models import load_model
+from helper import prepare_image_for_ela, prerpare_img_for_weather
+from fetchOriginal import image_coordinates, get_weather
+from PIL import Image as PILImage
+import io
+
+# Constants
+class_weather = ['Lightning', 'Rainy', 'Snow', 'Sunny']
+class_ELA = ['Real', 'Tampered']
+outdoor = False
+
+# Functions
+def check_img(image_name):
+    img = cv2.resize(cv2.imread(image_name), (750, 750))
+    return img
+
+def detect_ELA(img_name):
+    global ela_result
+    np_img_input, ela_result = prepare_image_for_ela(img_name)
+    ELA_model = load_model('ELA_Training/model_ela.h5')
+    Y_predicted = ELA_model.predict(np_img_input, verbose=0)
+    return "Model shows {}% accuracy of image being {}".format(round(np.max(Y_predicted[0]) * 100), class_ELA[np.argmax(Y_predicted[0])])
+
+def detect_weather(img_name):
+    np_img_input = prerpare_img_for_weather(img_name)
+    model_Weather = load_model('WeatherCNNTraining/Weather_Model.h5')
+    Y_predicted = model_Weather.predict(np_img_input, verbose=0)
+    return "Model shows weather in Image is {} ({}% accurate)".format(class_weather[np.argmax(Y_predicted[0])],round(np.max(Y_predicted[0]) * 100))
+
+def org_weather(img_name):
+    global outdoor
+    date_time, lat, long, outdoor = image_coordinates(img_name)
+    if not outdoor:
+        return
+    print(lat)
+    location, date, weather = get_weather(date_time, lat, long)
+    if lat == 0.0:
+        return "Not able to fetch location. (some devices may mask location exif info)."
+    if weather == "NA":
+        return "The Image was taken at {} on {}; weather data not available".format(location, date)
+    return "The Image was taken at {} and weather there on {} was {}".format(location, date, weather)
+
+# Streamlit Interface
+st.title("Image Tampering Detection Using ELA and Metadata Analysis")
+
+st.markdown(""" 
+            ### Welcome to the Image Tampering Detector!
+            **What This Project Does:**
+            This tool helps you determine whether an image has been altered or manipulated. Try it out and see if your images hold up under scrutiny!
+            """)
+with st.expander("How it Works?"):
+    st.markdown("""
+There are two parallels that are being used to identify an image's authenticity:
+
+1. **Error Level Analysis (ELA):**
+   - Images that are edited often show different compression artifacts compared to the original ones. ELA highlights these discrepancies, making tampering visible.
+""")
+
+    # Create columns for horizontal layout
+    col1, col2 = st.columns(2)
+
+    # Display ELA images in columns
+    with col1:
+        st.image("rsc/real.jpg", caption="ELA of a Real Image", use_column_width=True)
+
+    with col2:
+        st.image("rsc/fake.jpg", caption="ELA of a Tampered Image", use_column_width=True)
+
+    st.markdown("""
+    Notice how ELA on real image shows consistency across the image but on fake image, ELA shows discrepancy across some regions.
+                
+    2. **Weather Validation:**
+        - **Purpose:** Weather Validation examines the metadata embedded in the image to cross-check if the depicted weather conditions are consistent with historical weather data from the image's recorded location and time.
+        - **How?:**
+            Images often contain metadata that includes information about the location, date, and time when the image was taken.
+            Using this metadata, historical weather databases are queried to retrieve the weather conditions for the recorded location and date.
+            Then actual weather conditions in the image are compared (as identified by a Weather CNN model) with the historical data to determine if they match.
+        - **Why It Matters:** This helps in verifying the authenticity of outdoor images by ensuring that the weather depicted matches what was recorded historically for that location and time. Discrepancies can indicate tampering or misrepresentation.
+""")
+with st.expander("How to use it?"):
+    st.markdown("""
+    1. **Upload an Image:** Choose an image to analyze.
+    2. **Select the Outdoor Option:** Indicate whether the image is taken outdoors.
+    3. **View Results:** Get insights into the image’s authenticity with detailed analysis.
+
+    """)
+
+with st.expander("Got a Query?"):
+    st.markdown("""
+Hi, I'm RIMJHIM
+                
+If you have any questions, feedback, suggestions, or just want to chat, feel free to reach out!
+
+- [Portfolio](https://portfolio-nextjs-five-plum.vercel.app/)
+- [Github](https://www.linkedin.com/in/rimjhim-jha-5114s80/)
+
+you can also email me at rj05jharimjhim@gmail.com.
+""")
+
+
+# Initialize session state
+if 'step' not in st.session_state:
+    st.session_state.step = 0
+
+# Handle file upload
+uploaded_file = st.file_uploader("Choose a .jpg/.jpeg image...", type=["jpg", "jpeg"])
+
+if uploaded_file is not None:
+    with open("temp.jpg", "wb") as f:
+        f.write(uploaded_file.getbuffer())
+
+    st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
+    flag = st.radio("Is weather visible in the image? (requires image metadata)", ('Yes', 'No'))
+
+    if st.button("Proceed") and st.session_state.step == 0:
+        st.session_state.step = 1
+
+# Show results if step > 0
+if st.session_state.step > 0:
+    org = check_img("temp.jpg")
+    res1 = detect_ELA("temp.jpg")
+    res2, res3 = '', ''
+    if flag == 'Yes':
+        res2 = org_weather("temp.jpg")
+        res3 = detect_weather("temp.jpg")
+
+    st.write("### Results:")
+    st.write("1. " + res1)
+    if flag == "Yes":
+        if not outdoor:
+            st.write("Trouble fetching exif. Image do not have location or time metadata. (some devices may mask location exif info).")
+        else:
+            st.write("2. " + res2)
+            st.write("3. " + res3) 
+
+    if st.button("Show Error Level Analysis") and st.session_state.step == 1:
+        # Display ELA result image
+        st.image(ela_result, caption="ELA Analysis")
+
+        #TODO ela img goes away, add session step here
+        
+        # Save ELA result image to a BytesIO object
+        buffer = io.BytesIO()
+        ela_result.save(buffer, format="JPEG")  # ela_result should be a PIL Image
+        buffer.seek(0)
+        
+        # Add a download button for the ELA result image
+        st.download_button(
+            label="Save ELA Result Image",
+            data=buffer,
+            file_name="ela_result.jpg",
+            mime="image/jpeg"
+        )
+
+    # Button to reset the session state and allow new image upload
+    if st.button("Try New Image"):
+        st.session_state.step = 0
+        #st.experimental_rerun() is not working
+        st.rerun()
